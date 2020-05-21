bdf75d2425b5a77fe81582680eb9c44bc1230f46
zidol <ajh3166@naver.com>
2020-05-21 23:53:40 +0900

    setting init

---
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -0,0 +1,24 @@
+package kr.co.fastcampus.eatgo.domain;
+public class Restaurant {
+    private final String name;
+    private final String address;
+    private final Long id;
+
+    public Restaurant(Long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return "Bob zip";
+    }
+    public String getAddress() {
+        return  address;
+    }
+    public String getInformation() {
+        return  name + " in " + address;
+    }
+}
---
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTests.java
@@ -0,0 +1,34 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@RunWith(SpringRunner.class)
+@WebMvcTest(RestController.class)
+public class RestaurantControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void list() throws Exception {
+        mvc.perform(get("/restaurant"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":1004")
+                ))
+                .andExpect(content().string(
+                        containsString("\"name\":\"Bob zip\"")
+                ));
+
+    }
+}
