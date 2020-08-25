9601725d3445fa01f71445b126421a7c014804ae
zidol <ajh3166@naver.com>
2020-08-26 01:32:30 +0900

    가게 목록 필터링 구현중 지역(region) 목록 테스트 구현

---
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RegionController.java
@@ -0,0 +1,26 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import com.sun.media.sound.DLSInstrument;
+import kr.co.fastcampus.eatgo.applicaton.RegionService;
+import kr.co.fastcampus.eatgo.domain.Region;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RegionController {
+
+    @Autowired
+    private RegionService regionsService;
+
+    @GetMapping("/regions")
+    public List<Region> list() {
+        List<Region> regions = regionsService.getRegions();
+//        List<Region> regions = new ArrayList<>();
+//        regions.add(Region.builder().name("Seoul").build());
+        return  regions;
+    }
+}
