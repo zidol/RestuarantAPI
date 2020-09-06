dd8920d68ff1c158e5007c4c3730b253c516dda0
zidol <ajh3166@naver.com>
2020-09-06 22:57:39 +0900

    jwt, filter를 통해 spring 내부 authentication 로직

--- eatgo-common/src/main/java/kr/co/fastcampus/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/utils/JwtUtil.java
@@ -1,5 +1,7 @@
 package kr.co.fastcampus.eatgo.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -16,11 +18,18 @@ public class JwtUtil {
     }
 
     public String createToken(Long userId, String name) {
-        String token = Jwts.builder()
+        return Jwts.builder()
                 .claim("userId", userId)
                 .claim("name", name)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
-        return token;
+    }
+
+    public Claims getClaims(String token) {
+
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)      //jws : sign이 포함된 jwt를 의미함
+                .getBody();
     }
 }
