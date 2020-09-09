b7e97d481a0e794a44ee781e4295b646ba141024
zidol <ajh3166@naver.com>
2020-09-10 01:06:31 +0900

    Reservation domain, service class 생성

--- eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/applicaton/ReservationService.java
+++ eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/applicaton/ReservationService.java
@@ -7,7 +7,7 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class ReservationService {
-    public void addReservation(Long userId, String name, String date, String time, Integer partySize) {
+    public void addReservation(Long restaurantId, Long userId, String name, String date, String time, Integer partySize) {
         //TODO : logic
     }
 }
