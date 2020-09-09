9b0c9d9252a05701ea2f3b188397a44af8a88b17
zidol <ajh3166@naver.com>
2020-09-10 01:05:59 +0900

    Reservation Controller, test 로직

---
+++ eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/applicaton/ReservationService.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.eatgo.applicaton;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class ReservationService {
+    public void addReservation(Long userId, String name, String date, String time, Integer partySize) {
+        //TODO : logic
+    }
+}
