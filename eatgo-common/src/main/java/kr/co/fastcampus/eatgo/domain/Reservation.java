4d1bdb2ca9f38f99c7f3013f8ef31e1c82483bfb
zidol <ajh3166@naver.com>
2020-09-11 00:26:32 +0900

    reservation domain에 restrauntId 추가

--- eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/Reservation.java
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/Reservation.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -18,6 +19,8 @@ public class Reservation {
     @Id
     @GeneratedValue
     private Long id;
+
+    private Long restaurantId;
 
     private Long userId;
 
@@ -34,5 +37,6 @@ public class Reservation {
 
     @Setter
     @NotNull
+    @Min(1)
     private Integer partySize;
 }
